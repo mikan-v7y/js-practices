eaf47c1c33b6b5ec031dc26dcd02432ed3e5ba05
mikan-v7y <mikan.v7y@gmail.com>
2025-09-04 05:10:43 +0900

    feat: 全ての文字が色無しで出力されるプログラムを実装

--- 01.fizzbuzz/fizzbuzz.js
+++ 01.fizzbuzz/fizzbuzz.js
@@ -9,3 +9,22 @@ for (let i = 1; i <= 20; i++) {
     console.log(i);
   }
 }
+
+console.log('整数は色付きで表示されます')
+console.log('')
+
+// 数字も色無しで出力する
+
+for (let i = 1; i <= 20; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz");
+  } else if (i % 3 === 0) {
+    console.log("Fizz");
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(i.toString());
+  }
+}
+
+console.log('全ての値が色無しで表示されます')
