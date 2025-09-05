84d65be0894ba5b66d6cb980bb6fcd005984b93f
mikan-v7y <mikan.v7y@gmail.com>
2025-09-06 03:03:40 +0900

    test: fizzbuzz-functionの変更に伴い、テストコードを修正

--- 01.fizzbuzz/fizzbuzz.test.js
+++ 01.fizzbuzz/fizzbuzz.test.js
@@ -14,6 +14,6 @@ test("15の倍数のときは、数の代わりにFizzBuzzと表示", () => {
 });
 
 test("それ以外は整数で表示", () => {
-  expect(fizzbuzz(1)).toBe(1);
-  expect(fizzbuzz(2)).toBe(2);
+  expect(fizzbuzz(1)).toBe("1");
+  expect(fizzbuzz(2)).toBe("2");
 });
