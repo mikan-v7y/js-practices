be39bef109b8923865c8e77b9d32895bc28da7d3
mikan-v7y <mikan.v7y@gmail.com>
2025-09-06 16:34:49 +0900

    refactor: 可読性向上のため、ループカウンタの変数名を i から number　に修正

--- 01.fizzbuzz/fizzbuzz.js
+++ 01.fizzbuzz/fizzbuzz.js
@@ -1,14 +1,14 @@
 #!/usr/bin/env node
 
-for (let i = 1; i <= 20; i++) {
-  if (i % 3 === 0 && i % 5 === 0) {
+for (let number = 1; number <= 20; number++) {
+  if (number % 3 === 0 && number % 5 === 0) {
     console.log("FizzBuzz");
-  } else if (i % 3 === 0) {
+  } else if (number % 3 === 0) {
     console.log("Fizz");
-  } else if (i % 5 === 0) {
+  } else if (number % 5 === 0) {
     console.log("Buzz");
   } else {
-    console.log(i.toString());
+    console.log(number.toString());
   }
 }
 
