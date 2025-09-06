6e96f1cdbf2835e3f7d8adf71ab10ab4d4ba9687
mikan-v7y <mikan.v7y@gmail.com>
2025-09-06 16:10:28 +0900

    refactor: 可読性向上のため、テスト名を修正

--- 01.fizzbuzz/fizzbuzz.test.js
+++ 01.fizzbuzz/fizzbuzz.test.js
@@ -1,19 +1,19 @@
 import fizzbuzz from "./fizzbuzz-function.js";
 
-test("3の倍数のときは、数の代わりにFizzと表示", () => {
+test("3の倍数かつ5の倍数でないときは、数の代わりにFizzを返す", () => {
   expect(fizzbuzz(3)).toBe("Fizz");
 });
 
-test("5の倍数のときは、数の代わりにBuzzと表示", () => {
+test("5の倍数かつ3の倍数でないときは、数の代わりにBuzzを返す", () => {
   expect(fizzbuzz(5)).toBe("Buzz");
   expect(fizzbuzz(20)).toBe("Buzz");
 });
 
-test("15の倍数のときは、数の代わりにFizzBuzzと表示", () => {
+test("3と5の両方の倍数のときは、数の代わりにFizzBuzzを返す", () => {
   expect(fizzbuzz(15)).toBe("FizzBuzz");
 });
 
-test("それ以外は整数で表示", () => {
+test("それ以外は数の文字列を返す", () => {
   expect(fizzbuzz(1)).toBe("1");
   expect(fizzbuzz(2)).toBe("2");
 });
