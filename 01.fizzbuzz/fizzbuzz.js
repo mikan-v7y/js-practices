a9a0b1cbed6befa2b9780cce474fbf5868e41801
mikan-v7y <mikan.v7y@gmail.com>
2025-09-08 21:40:10 +0900

    refactor: fizzbuzz.jsをテストするため、関数を使った処理に変更

--- 01.fizzbuzz/fizzbuzz.js
+++ 01.fizzbuzz/fizzbuzz.js
@@ -1,13 +1,21 @@
 #!/usr/bin/env node
 
-for (let number = 1; number <= 20; number++) {
+export function fizzbuzz(number) {
   if (number % 3 === 0 && number % 5 === 0) {
-    console.log("FizzBuzz");
+    return "FizzBuzz";
   } else if (number % 3 === 0) {
-    console.log("Fizz");
+    return "Fizz";
   } else if (number % 5 === 0) {
-    console.log("Buzz");
+    return "Buzz";
   } else {
-    console.log(number.toString());
+    return number.toString();
   }
 }
+
+function main() {
+  for (let number = 1; number <= 20; number++) {
+    console.log(fizzbuzz(number));
+  }
+}
+
+main();
