79c5ceb0171591752c1dca1b831c3cb81209de20
mikan-v7y <mikan.v7y@gmail.com>
2025-10-04 03:34:17 +0900

    コールバック形式:エラーなし:可読性向上のため、変数名 statement を insertBookStmt に変更

--- 03.asynchronous/callback/success.js
+++ 03.asynchronous/callback/success.js
@@ -5,16 +5,16 @@ const db = new sqlite3.Database(":memory:");
 db.run(
   "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
   function () {
-    const statement = db.prepare("INSERT INTO books (title) VALUES (?)");
+    const insertBookStmt = db.prepare("INSERT INTO books (title) VALUES (?)");
 
-    statement.run("チェリー本", function () {
+    insertBookStmt.run("チェリー本", function () {
       console.log(`ID${this.lastID}が自動採番されました。`);
     });
 
-    statement.run("ゼロからわかるRuby超入門", function () {
+    insertBookStmt.run("ゼロからわかるRuby超入門", function () {
       console.log(`ID${this.lastID}が自動採番されました。`);
     });
 
-    statement.finalize();
+    insertBookStmt.finalize();
   },
 );
