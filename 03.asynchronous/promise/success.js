1df9cfff65fe230f2f28db9165a62c7686fd7a38
mikan-v7y <mikan.v7y@gmail.com>
2025-10-06 23:15:25 +0900

    Promiseでdb.runをラップする asyncFunc 関数を追加

--- 03.asynchronous/promise/success.js
+++ 03.asynchronous/promise/success.js
@@ -1,3 +1,11 @@
 import sqlite3 from "sqlite3";
 
 const db = new sqlite3.Database(":memory:");
+
+function asyncFunc(sql, params) {
+  return new Promise((resolve) => {
+    db.run(sql, params, function () {
+      resolve(this);
+    });
+  });
+}
