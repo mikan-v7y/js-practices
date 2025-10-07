5dcba687ea0ce967e0f45abe3e3e8b3273a57410
mikan-v7y <mikan.v7y@gmail.com>
2025-10-07 15:59:54 +0900

    Promise形式:エラーなし:レコードを追加し、自動採番されたIDを標準出力する

--- 03.asynchronous/promise/success.js
+++ 03.asynchronous/promise/success.js
@@ -2,7 +2,7 @@ import sqlite3 from "sqlite3";
 
 const db = new sqlite3.Database(":memory:");
 
-function asyncFunc(sql, params) {
+function runSqlAsync(sql, params) {
   return new Promise((resolve) => {
     db.run(sql, params, function () {
       resolve(this);
@@ -10,6 +10,18 @@ function asyncFunc(sql, params) {
   });
 }
 
-asyncFunc(
+runSqlAsync(
   "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-);
+)
+  .then(() => {
+    return runSqlAsync("INSERT INTO books (title) VALUES (?)", ["チェリー本"]);
+  })
+  .then((result) => {
+    console.log(`ID${result.lastID}が自動採番されました。`);
+    return runSqlAsync("INSERT INTO books (title) VALUES (?)", [
+      "ゼロからわかるRuby超入門",
+    ]);
+  })
+  .then((result) => {
+    console.log(`ID${result.lastID}が自動採番されました。`);
+  });
