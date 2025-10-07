4525b7eea7a5cc447f72153b76c8e88963521160
mikan-v7y <mikan.v7y@gmail.com>
2025-10-08 01:33:17 +0900

    Promise形式:エラーあり:booksテーブルを作成

--- 03.asynchronous/promise/error.js
+++ 03.asynchronous/promise/error.js
@@ -1,3 +1,14 @@
 import sqlite3 from "sqlite3";
 
 const db = new sqlite3.Database(":memory:");
+
+function runSqlAsync(sql, params) {
+  return new Promise((resolve, reject) => {
+    db.run(sql, params, function (err) {
+      if (err) reject(err);
+      else resolve(this);
+    });
+  });
+}
+
+runSqlAsync("CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)");
