2e4b4b4e2b0d08c070d06efb1be4aae8c49d925c
mikan-v7y <mikan.v7y@gmail.com>
2025-10-09 01:31:18 +0900

    async/await形式:エラーあり:booksテーブルを作成

--- 03.asynchronous/async-await/error.js
+++ 03.asynchronous/async-await/error.js
@@ -1,3 +1,9 @@
 import { runSqlAsync, eachSqlAsync, closeDb } from "../db.js";
 
-async function f() {}
+async function f() {
+  await runSqlAsync(
+    "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+  );
+}
+
+f();
