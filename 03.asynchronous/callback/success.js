697cf9daf42dd23a8b637c7d67b67c06aa837205
mikan-v7y <mikan.v7y@gmail.com>
2025-10-09 23:47:33 +0900

    コールバック形式:エラーなし:レコードを取得し、それを標準出力に出力する

--- 03.asynchronous/callback/success.js
+++ 03.asynchronous/callback/success.js
@@ -5,18 +5,18 @@ const db = new sqlite3.Database(":memory:");
 db.run(
   "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
   function () {
-    const statement = db.prepare("INSERT INTO books (title) VALUES (?)");
-
     const insertBookStmt = db.prepare("INSERT INTO books (title) VALUES (?)");
 
     insertBookStmt.run("チェリー本", function () {
       console.log(`ID${this.lastID}が自動採番されました。`);
-    });
 
-    insertBookStmt.run("ゼロからわかるRuby超入門", function () {
-      console.log(`ID${this.lastID}が自動採番されました。`);
-    });
+      insertBookStmt.run("ゼロからわかるRuby超入門", function () {
+        console.log(`ID${this.lastID}が自動採番されました。`);
 
-    insertBookStmt.finalize();
+        db.each("SELECT id, title FROM books", (_, row) => {
+          console.log(`{ID:${row.id}, タイトル:${row.title}}`);
+        });
+      });
+    });
   },
 );
