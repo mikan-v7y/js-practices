2aba0f5be22f493dc438cc23d21901cb2847f905
mikan-v7y <mikan.v7y@gmail.com>
2025-10-09 23:38:08 +0900

    コールバック形式:エラーなし:レコードを追加し、自動採番された ID を標準出力に出力する

--- 03.asynchronous/callback/success.js
+++ 03.asynchronous/callback/success.js
@@ -4,7 +4,19 @@ const db = new sqlite3.Database(":memory:");
 
 db.run(
   "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-);
+  function () {
+    const statement = db.prepare("INSERT INTO books (title) VALUES (?)");
 
-db.run("INSERT INTO books (title) VALUES ('チェリー本')");
-db.run("INSERT INTO books (title) VALUES ('ゼロからわかるRuby超入門')");
+    const insertBookStmt = db.prepare("INSERT INTO books (title) VALUES (?)");
+
+    insertBookStmt.run("チェリー本", function () {
+      console.log(`ID${this.lastID}が自動採番されました。`);
+    });
+
+    insertBookStmt.run("ゼロからわかるRuby超入門", function () {
+      console.log(`ID${this.lastID}が自動採番されました。`);
+    });
+
+    insertBookStmt.finalize();
+  },
+);
