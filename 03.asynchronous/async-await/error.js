726bc70a55c5c3d596f3050ef78df572e98de971
mikan-v7y <mikan.v7y@gmail.com>
2025-10-11 01:43:51 +0900

    async/await形式:エラーあり:エラーを伴うレコードの追加を行い、発生したエラーのエラーメッセージを標準出力する

--- 03.asynchronous/async-await/error.js
+++ 03.asynchronous/async-await/error.js
@@ -4,6 +4,12 @@ async function f() {
   await runSqlAsync(
     "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
   );
+
+  try {
+    await runSqlAsync("INSERT INTO books (title) VALUES (?)", [null]);
+  } catch (err) {
+    console.error(`エラーを伴うレコードの追加: ${err.message}`);
+  }
 }
 
 f();
