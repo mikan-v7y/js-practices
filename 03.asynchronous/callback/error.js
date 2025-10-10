cd80e9f16ef784b9a8f4ad89407d13e6a4aa1b34
mikan-v7y <mikan.v7y@gmail.com>
2025-10-10 21:54:03 +0900

    コールバック形式:エラーあり:テーブルを作成

---
+++ 03.asynchronous/callback/error.js
@@ -0,0 +1,7 @@
+import sqlite3 from "sqlite3";
+
+const db = new sqlite3.Database(":memory:");
+
+db.run(
+  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+);
