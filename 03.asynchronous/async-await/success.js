62b45bf7a4cddd5b1f4291ed063054ee5db4c54a
mikan-v7y <mikan.v7y@gmail.com>
2025-10-11 01:34:44 +0900

    async/await形式:エラーなし:レコードを追加し、自動採番されたIDを標準出力する

--- 03.asynchronous/async-await/success.js
+++ 03.asynchronous/async-await/success.js
@@ -4,6 +4,16 @@ async function f() {
   await runSqlAsync(
     "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
   );
+
+  const result1 = await runSqlAsync("INSERT INTO books (title) VALUES (?)", [
+    "チェリー本",
+  ]);
+  console.log(`ID${result1.lastID}が自動採番されました。`);
+
+  const result2 = await runSqlAsync("INSERT INTO books (title) VALUES (?)", [
+    "ゼロからわかるRuby超入門",
+  ]);
+  console.log(`ID${result2.lastID}が自動採番されました。`);
 }
 
 f();
