e1061a14bba526f131090a420bc8d8ad9cac09d6
mikan-v7y <mikan.v7y@gmail.com>
2025-10-11 00:31:20 +0900

    Promise形式:エラーあり:エラーを伴うレコードの取得を行い、発生したエラーのエラーメッセージを標準出力する

--- 03.asynchronous/promise/error.js
+++ 03.asynchronous/promise/error.js
@@ -11,6 +11,26 @@ function runSqlAsync(sql, params) {
   });
 }
 
+function eachSqlAsync(sql, params) {
+  return new Promise((resolve, reject) => {
+    db.each(
+      sql,
+      params,
+      (err, row) => {
+        if (err) {
+          reject(err);
+        } else {
+          console.log(row);
+        }
+      },
+      (err, count) => {
+        if (err) reject(err);
+        else resolve(count);
+      },
+    );
+  });
+}
+
 runSqlAsync(
   "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
 )
@@ -19,4 +39,10 @@ runSqlAsync(
   })
   .catch((err) => {
     console.error(`エラーを伴うレコードの追加: ${err.message}`);
+  })
+  .then(() => {
+    return eachSqlAsync("SELECT id, hogehoge FROM books");
+  })
+  .catch((err) => {
+    console.error(`エラーを伴うレコードの取得: ${err.message}`);
   });
