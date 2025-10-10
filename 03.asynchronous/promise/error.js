081a46d9b2e515d9cd2728c9c8e1749cedddac97
mikan-v7y <mikan.v7y@gmail.com>
2025-10-11 00:22:14 +0900

    Promise形式:エラーあり:エラーを伴うレコードの追加を行い、発生したエラーのエラーメッセージを標準出力する

--- 03.asynchronous/promise/error.js
+++ 03.asynchronous/promise/error.js
@@ -13,4 +13,10 @@ function runSqlAsync(sql, params) {
 
 runSqlAsync(
   "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-);
+)
+  .then(() => {
+    return runSqlAsync("INSERT INTO books (title) VALUES (?)", [null]);
+  })
+  .catch((err) => {
+    console.error(`エラーを伴うレコードの追加: ${err.message}`);
+  });
