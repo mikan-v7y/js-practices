ce8d5e168388c40b4a53f0865cdee282bfef9c69
mikan-v7y <mikan.v7y@gmail.com>
2025-10-10 21:56:34 +0900

    コールバック形式:エラーあり:エラーを伴うレコードの追加を行い、発生したエラーのエラーメッセージを標準出力する

--- 03.asynchronous/callback/error.js
+++ 03.asynchronous/callback/error.js
@@ -4,4 +4,13 @@ const db = new sqlite3.Database(":memory:");
 
 db.run(
   "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+  function () {
+    const insertBookStmt = db.prepare("INSERT INTO books (title) VALUES (?)");
+
+    insertBookStmt.run(null, function (err) {
+      if (err) {
+        console.error(`標準エラー: ${err.message}`);
+      }
+    });
+  },
 );
