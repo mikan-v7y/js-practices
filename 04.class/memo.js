2feb6eb4aa68a105e6a8c2b280aed2b938041b9c
mikan-v7y <mikan.v7y@gmail.com>
2025-10-17 04:26:57 +0900

    エントリーポイントを実装

---
+++ 04.class/memo.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+import Create from "./memoApp/create.js";
+import Delete from "./memoApp/delete.js";
+import List from "./memoApp/list.js";
+import Read from "./memoApp/read.js";
+import MemoOption from "./memoOption.js";
+import MemoStorage from "./memoStorage.js";
+
+async function main() {
+  const option = new MemoOption();
+  const storage = new MemoStorage();
+  await storage.connect();
+
+  switch (option.action) {
+    case "create":
+      await new Create(storage).run();
+      break;
+    case "list":
+      await new List(storage).run();
+      break;
+    case "read":
+      await new Read(storage).run();
+      break;
+    case "delete":
+      await new Delete(storage).run();
+      break;
+  }
+}
+
+main();
