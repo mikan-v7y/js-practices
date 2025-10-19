d6f59400ed69e64c5e5ca1a1972137a1db4a0b10
mikan-v7y <mikan.v7y@gmail.com>
2025-10-20 02:49:32 +0900

    refactor:thisを参照しない function 関数をアロー関数に修正

--- 03.asynchronous/callback/success.js
+++ 03.asynchronous/callback/success.js
@@ -4,7 +4,7 @@ const db = new sqlite3.Database(":memory:");
 
 db.run(
   "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-  function () {
+  () => {
     const insertBookStmt = db.prepare("INSERT INTO books (title) VALUES (?)");
 
     insertBookStmt.run("チェリー本", function () {
