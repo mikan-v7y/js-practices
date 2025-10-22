19348723422682382ba3373205e1998a19979e40
mikan-v7y <mikan.v7y@gmail.com>
2025-10-22 16:44:41 +0900

    refactor: async 関数の代わりに Top-level await を使用

--- 03.asynchronous/async-await/error.js
+++ 03.asynchronous/async-await/error.js
@@ -6,33 +6,27 @@ import {
   closeDb,
 } from "../db.js";
 
-async function f() {
-  await runSqlAsync(
-    db,
-    "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-  );
+await runSqlAsync(
+  db,
+  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+);
 
-  const insertBookStatement = db.prepare(
-    "INSERT INTO books (title) VALUES (?)",
-  );
+const insertBookStatement = db.prepare("INSERT INTO books (title) VALUES (?)");
 
-  try {
-    await runStatementAsync(insertBookStatement, [null]);
-  } catch (err) {
-    console.error(`エラーを伴うレコードの追加: ${err.message}`);
-  }
-
-  try {
-    await eachSqlAsync(db, "SELECT id, hogehoge FROM books");
-  } catch (err) {
-    console.error(`エラーを伴うレコードの取得: ${err.message}`);
-  }
-
-  insertBookStatement.finalize();
-
-  await runSqlAsync(db, "DROP TABLE books");
-
-  await closeDb(db);
+try {
+  await runStatementAsync(insertBookStatement, [null]);
+} catch (err) {
+  console.error(`エラーを伴うレコードの追加: ${err.message}`);
 }
 
-f();
+try {
+  await eachSqlAsync(db, "SELECT id, hogehoge FROM books");
+} catch (err) {
+  console.error(`エラーを伴うレコードの取得: ${err.message}`);
+}
+
+insertBookStatement.finalize();
+
+await runSqlAsync(db, "DROP TABLE books");
+
+await closeDb(db);
--- 03.asynchronous/async-await/success.js
+++ 03.asynchronous/async-await/success.js
@@ -6,33 +6,27 @@ import {
   closeDb,
 } from "../db.js";
 
-async function f() {
-  await runSqlAsync(
-    db,
-    "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-  );
+await runSqlAsync(
+  db,
+  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+);
 
-  const insertBookStatement = db.prepare(
-    "INSERT INTO books (title) VALUES (?)",
-  );
+const insertBookStatement = db.prepare("INSERT INTO books (title) VALUES (?)");
 
-  const result1 = await runStatementAsync(insertBookStatement, ["チェリー本"]);
-  console.log(`ID${result1.lastID}が自動採番されました。`);
+const result1 = await runStatementAsync(insertBookStatement, ["チェリー本"]);
+console.log(`ID${result1.lastID}が自動採番されました。`);
 
-  const result2 = await runStatementAsync(insertBookStatement, [
-    "ゼロからわかるRuby超入門",
-  ]);
-  console.log(`ID${result2.lastID}が自動採番されました。`);
+const result2 = await runStatementAsync(insertBookStatement, [
+  "ゼロからわかるRuby超入門",
+]);
+console.log(`ID${result2.lastID}が自動採番されました。`);
 
-  await eachSqlAsync(db, "SELECT id, title FROM books", [], (row) => {
-    console.log(`{ID:${row.id}, タイトル:${row.title}}`);
-  });
+await eachSqlAsync(db, "SELECT id, title FROM books", [], (row) => {
+  console.log(`{ID:${row.id}, タイトル:${row.title}}`);
+});
 
-  insertBookStatement.finalize();
+insertBookStatement.finalize();
 
-  await runSqlAsync(db, "DROP TABLE books");
+await runSqlAsync(db, "DROP TABLE books");
 
-  await closeDb(db);
-}
-
-f();
+await closeDb(db);
