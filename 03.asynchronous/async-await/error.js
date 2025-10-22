ecfa26d4ba9f9217a96dfc3ab18431e0dc89e969
mikan-v7y <mikan.v7y@gmail.com>
2025-10-22 17:36:22 +0900

    refactor: 捕捉したい例外のみを捕捉するように変更

--- 03.asynchronous/async-await/error.js
+++ 03.asynchronous/async-await/error.js
@@ -6,27 +6,39 @@ import {
   closeDb,
 } from "../db.js";
 
-await runSqlAsync(
-  db,
-  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-);
-
-const insertBookStatement = db.prepare("INSERT INTO books (title) VALUES (?)");
-
 try {
-  await runStatementAsync(insertBookStatement, [null]);
+  await runSqlAsync(
+    db,
+    "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+  );
+
+  const insertBookStatement = db.prepare(
+    "INSERT INTO books (title) VALUES (?)",
+  );
+
+  try {
+    await runStatementAsync(insertBookStatement, [null]);
+  } catch (err) {
+    if (err.code === "SQLITE_CONSTRAINT") {
+      console.error(`エラーを伴うレコードの追加: ${err.message}`);
+    } else {
+      throw err;
+    }
+  }
+
+  try {
+    await eachSqlAsync(db, "SELECT id, hogehoge FROM books");
+  } catch (err) {
+    if (err.code === "SQLITE_ERROR") {
+      console.error(`エラーを伴うレコードの取得: ${err.message}`);
+    } else {
+      throw err;
+    }
+  }
+
+  insertBookStatement.finalize();
+  await runSqlAsync(db, "DROP TABLE books");
+  await closeDb(db);
 } catch (err) {
-  console.error(`エラーを伴うレコードの追加: ${err.message}`);
+  console.error(`想定外のエラー: ${err.message}`);
 }
-
-try {
-  await eachSqlAsync(db, "SELECT id, hogehoge FROM books");
-} catch (err) {
-  console.error(`エラーを伴うレコードの取得: ${err.message}`);
-}
-
-insertBookStatement.finalize();
-
-await runSqlAsync(db, "DROP TABLE books");
-
-await closeDb(db);
