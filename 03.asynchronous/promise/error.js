0c0089ae0c438767eec2b0d5ca15df69eb61cc32
mikan-v7y <mikan.v7y@gmail.com>
2025-11-18 21:43:19 +0900

    refactor: 可読性向上のため、Promise チェーンのネストを解消

--- 03.asynchronous/promise/error.js
+++ 03.asynchronous/promise/error.js
@@ -9,27 +9,27 @@ import sqlite3 from "sqlite3";
 
 const db = new sqlite3.Database(":memory:");
 
+let insertBookStatement;
+
 runSqlAsync(
   db,
   "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-).then(() => {
-  const insertBookStatement = db.prepare(
-    "INSERT INTO books (title) VALUES (?)",
-  );
+)
+  .then(() => {
+    insertBookStatement = db.prepare("INSERT INTO books (title) VALUES (?)");
+    return runStatementAsync(insertBookStatement, [null]);
+  })
+  .catch((err) => {
+    console.error(`エラーを伴うレコードの追加: ${err.message}`);
 
-  return runStatementAsync(insertBookStatement, [null])
-    .catch((err) => {
-      console.error(`エラーを伴うレコードの追加: ${err.message}`);
+    return eachSqlAsync(db, "SELECT id, hogehoge FROM books");
+  })
+  .catch((err) => {
+    console.error(`エラーを伴うレコードの取得: ${err.message}`);
 
-      return eachSqlAsync(db, "SELECT id, hogehoge FROM books");
-    })
-    .catch((err) => {
-      console.error(`エラーを伴うレコードの取得: ${err.message}`);
-
-      insertBookStatement.finalize();
-      return runSqlAsync(db, "DROP TABLE books");
-    })
-    .then(() => {
-      closeDb(db);
-    });
-});
+    insertBookStatement.finalize();
+    return runSqlAsync(db, "DROP TABLE books");
+  })
+  .then(() => {
+    closeDb(db);
+  });
--- 03.asynchronous/promise/success.js
+++ 03.asynchronous/promise/success.js
@@ -9,35 +9,29 @@ import sqlite3 from "sqlite3";
 
 const db = new sqlite3.Database(":memory:");
 
+let insertBookStatement;
+
 runSqlAsync(
   db,
   "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
 )
   .then(() => {
-    const insertBookStatement = db.prepare(
-      "INSERT INTO books (title) VALUES (?)",
-    );
-
-    return runStatementAsync(insertBookStatement, ["チェリー本"])
-      .then((result) => {
-        console.log(`ID${result.lastID}が自動採番されました。`);
-
-        return runStatementAsync(insertBookStatement, [
-          "ゼロからわかるRuby超入門",
-        ]);
-      })
-      .then((result) => {
-        console.log(`ID${result.lastID}が自動採番されました。`);
-
-        return eachSqlAsync(db, "SELECT id, title FROM books", [], (row) => {
-          console.log(`{ID:${row.id}, タイトル:${row.title}}`);
-        });
-      })
-      .then(() => {
-        insertBookStatement.finalize();
-
-        return runSqlAsync(db, "DROP TABLE books");
-      });
+    insertBookStatement = db.prepare("INSERT INTO books (title) VALUES (?)");
+    return runStatementAsync(insertBookStatement, ["チェリー本"]);
+  })
+  .then((result) => {
+    console.log(`ID${result.lastID}が自動採番されました。`);
+    return runStatementAsync(insertBookStatement, ["ゼロからわかるRuby超入門"]);
+  })
+  .then((result) => {
+    console.log(`ID${result.lastID}が自動採番されました。`);
+    return eachSqlAsync(db, "SELECT id, title FROM books", [], (row) => {
+      console.log(`{ID:${row.id}, タイトル:${row.title}}`);
+    });
+  })
+  .then(() => {
+    insertBookStatement.finalize();
+    return runSqlAsync(db, "DROP TABLE books");
   })
   .then(() => {
     closeDb(db);
