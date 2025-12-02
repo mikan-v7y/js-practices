30772e785514f0a741cfb66fc872b03e6c3ffd08
mikan-v7y <mikan.v7y@gmail.com>
2025-12-03 00:13:26 +0900

    refactor: db プロパティを private にして外部からアクセスできないように変更

--- 04.class/memoStorage.js
+++ 04.class/memoStorage.js
@@ -2,17 +2,15 @@ import sqlite3 from "sqlite3";
 import { open } from "sqlite";
 
 export default class MemoStorage {
-  constructor() {
-    this.db = null;
-  }
+  #db = null;
 
   async connect() {
-    if (this.db === null) {
-      this.db = await open({
+    if (this.#db === null) {
+      this.#db = await open({
         filename: "./memos.db",
         driver: sqlite3.Database,
       });
-      await this.db.exec(`
+      await this.#db.exec(`
         CREATE TABLE IF NOT EXISTS memos (
           id INTEGER PRIMARY KEY AUTOINCREMENT,
           content TEXT NOT NULL
@@ -22,18 +20,18 @@ export default class MemoStorage {
   }
 
   async insert(content) {
-    await this.db.run("INSERT INTO memos (content) VALUES (?)", content);
+    await this.#db.run("INSERT INTO memos (content) VALUES (?)", content);
   }
 
   async getAll() {
-    return await this.db.all("SELECT * FROM memos");
+    return await this.#db.all("SELECT * FROM memos");
   }
 
   async getById(id) {
-    return await this.db.get("SELECT * FROM memos WHERE id = ?", id);
+    return await this.#db.get("SELECT * FROM memos WHERE id = ?", id);
   }
 
   async deleteById(id) {
-    await this.db.run("DELETE FROM memos WHERE id = ?", id);
+    await this.#db.run("DELETE FROM memos WHERE id = ?", id);
   }
 }
