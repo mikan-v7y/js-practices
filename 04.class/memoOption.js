e1e690b85aca3c5bcff1c11fe83b6dc796bf4466
mikan-v7y <mikan.v7y@gmail.com>
2025-12-02 22:27:50 +0900

    refactor: プロパティである action を private に変更し、外部からアクセスする時は getter を使用するように変更

--- 04.class/memoOption.js
+++ 04.class/memoOption.js
@@ -2,15 +2,21 @@ import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
 
 export default class MemoOption {
-  constructor() {
-    const argv = yargs(hideBin(process.argv))
+  #action;
+
+  constructor(argv = process.argv) {
+    const parsed = yargs(hideBin(argv))
       .option("l", { type: "boolean" })
       .option("r", { type: "boolean" })
       .option("d", { type: "boolean" }).argv;
 
-    if (argv.l) this.action = "list";
-    else if (argv.r) this.action = "read";
-    else if (argv.d) this.action = "delete";
-    else this.action = "create";
+    if (parsed.l) this.#action = "list";
+    else if (parsed.r) this.#action = "read";
+    else if (parsed.d) this.#action = "delete";
+    else this.#action = "create";
+  }
+
+  get action() {
+    return this.#action;
   }
 }
